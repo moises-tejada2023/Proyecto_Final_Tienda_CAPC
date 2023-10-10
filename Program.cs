e8200f17cddba2c2c16cdea7ab88173e2d191055
Moisés Tejada <moises.tejada23@itca.edu.sv>
2023-10-09 19:59:53 -0600

    Diseño de meú agregado

--- Program.cs
+++ Program.cs
@@ -1,1 +1,105 @@
-﻿
+﻿//Uso de vectores para almacenar los productos
+string[] productos = new string[14];
+string[] productosParaMenu = new string[14];
+decimal[] precio = new decimal[14];
+decimal[] cantidad = new decimal[14];
+string[] tipoCanPro = new string[14];
+
+//Asignación de valores
+
+tipoCanPro[0] = "lb         ";
+tipoCanPro[1] = "lb         ";
+tipoCanPro[2] = "lb         ";
+tipoCanPro[3] = "lb         ";
+tipoCanPro[4] = "caja       ";
+tipoCanPro[5] = "paquete    ";
+tipoCanPro[6] = "galón      ";
+tipoCanPro[7] = "bolsa      ";
+tipoCanPro[8] = "bolsa      ";
+tipoCanPro[9] = "paquete    ";
+tipoCanPro[10] = "paquete    ";
+tipoCanPro[11] = "unidad     ";
+tipoCanPro[12] = "unidad     ";
+tipoCanPro[13] = "paquete    ";
+
+productos[0] = "Jamón de Pavo.";
+productos[1] = "Carne Pollo.";
+productos[2] = "Carne Res.";
+productos[3] = "Carne Cerdo.";
+productos[4] = "Cereal Integral.";
+productos[5] = "Galletas Oreo.";
+productos[6] = "Leche Salud.";
+productos[7] = "Detergente Xedex.";
+productos[8] = "Limpia pisos Fabuloso.";
+productos[9] = "Pasta de Dientes Colgate.";
+productos[10] = "Rasuradoras Lady Speed Stick.";
+productos[11] = "Almohada para sala.";
+productos[12] = "Muñeca Barbie Mattel.";
+productos[13] = "Lapiceros Bic.";
+
+productosParaMenu[0] = "Jamón de Pavo.                   ";
+productosParaMenu[1] = "Carne Pollo.                     ";
+productosParaMenu[2] = "Carne Res.                       ";
+productosParaMenu[3] = "Carne Cerdo.                     ";
+productosParaMenu[4] = "Cereal Integral.                 ";
+productosParaMenu[5] = "Galletas Oreo.                   ";
+productosParaMenu[6] = "Leche Salud.                     ";
+productosParaMenu[7] = "Detergente Xedex.                ";
+productosParaMenu[8] = "Limpia pisos Fabuloso.           ";
+productosParaMenu[9] = "Pasta de Dientes Colgate.        ";
+productosParaMenu[10] = "Rasuradoras Lady Speed Stick.    ";
+productosParaMenu[11] = "Almohada para sala.              ";
+productosParaMenu[12] = "Muñeca Barbie Mattel.            ";
+productosParaMenu[13] = "Lapiceros Bic.                   ";
+
+precio[0] = 2.15m;
+precio[1] = 2.50m;
+precio[2] = 3.50m;
+precio[3] = 3.25m;
+precio[4] = 3.00m;
+precio[5] = 4.25m;
+precio[6] = 6.70m;
+precio[7] = 5.00m;
+precio[8] = 3.30m;
+precio[9] = 1.75m;
+precio[10] = 3.99m;
+precio[11] = 6.00m;
+precio[12] = 15.25m;
+precio[13] = 2.20m;
+
+cantidad[0] = 220m;
+cantidad[1] = 500m;
+cantidad[2] = 440m;
+cantidad[3] = 360m;
+cantidad[4] = 77m;
+cantidad[5] = 125m;
+cantidad[6] = 57m;
+cantidad[7] = 187m;
+cantidad[8] = 110m;
+cantidad[9] = 74m;
+cantidad[10] = 80m;
+cantidad[11] = 150m;
+cantidad[12] = 43m;
+cantidad[13] = 230m;
+
+
+//Creación de la pantalla de Bienvenida
+Console.Clear();
+Console.WriteLine("\n                      *****      BIENVENIDO A LA TIENDA       *****");
+Console.WriteLine("                      *****          SUPER PRECIOS            *****\n");
+
+Console.WriteLine("                          ... Pulse una tecla para continuar ...");
+Console.ReadKey();
+
+//Menú de compras
+Console.WriteLine("\n            **********            MENÚ DE COMPPRAS            **********\n");
+Console.Write("    UNIDAD VENTA      ");
+Console.Write("PRODUCTO      ");
+Console.Write("                   PRECIO      ");
+Console.WriteLine("    CANTIDAD   ");
+
+for (int i = 0; i < 14; i++) {
+    Console.WriteLine("    " + tipoCanPro[i] + "       " + productosParaMenu[i] + "$" + precio[i] + "           " + cantidad[i]);
+}
+
+Console.WriteLine("");
